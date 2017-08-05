b9c77808d6ac3a1ae4b252132dd8de3644905583
by46 <ycs_ctbu_2010@126.com>
2017-08-05 16:56:16 +0800

    feat: add mock code for main loop

--- tds/parser.py
+++ tds/parser.py
@@ -18,10 +18,16 @@ class Parser(object):
     }
 
     def __init__(self, conn, address, logger=None):
+        """
+        
+        :param socket conn: 
+        :param (str, int) address: 
+        :param logger: 
+        """
         self.logger = logger or logging.getLogger(__name__)
         self.conn = conn
         self.client_ip, self.client_port = address
-        self.conn = None  # type: socket
+        self.conn = conn
         self.user = None
         self.database = None
         self.settings = {}
@@ -29,12 +35,17 @@ class Parser(object):
     def run(self):
         while True:
             try:
-                pass
+                # TODO(benjamin): testing code
+                client_content = self.conn.recv(10)
+                self.logger.info('Receive from client, %s', client_content)
+                self.conn.send('1234567890')
+                self.conn.close()
+                raise AbortException()
             except AbortException as e:
                 # TODO(benjamin): process abort exception
                 self.logger.exception(e)
                 # TODO(benjamin): send logout event
-                pass
+                break
 
     def parse_message_header(self, conn=None):
         """
