72da2df19f88947df4ba32c0f04b6da7684769a7
by46 <ycs_ctbu_2010@126.com>
2017-08-05 16:38:34 +0800

    feat: add init method for Parse class

--- tds/parser.py
+++ tds/parser.py
@@ -1,3 +1,22 @@
+import logging
+from socket import socket
+
+EVENT_LOGIN = "login"
+EVENT_LOGOUT = "logout"
+EVENT_INPUT = "input"
+EVENT_OUTPUT = "output"
+EVENT_BATCH = "batch"
+
+
 class Parser(object):
+    def __init__(self, conn, address, logger=None):
+        self.logger = logger or logging.getLogger(__name__)
+        self.conn = conn
+        self.client_ip, self.client_port = address
+        self.conn = None  # type: socket
+        self.user = None
+        self.database = None
+        self.settings = {}
+
     def run(self):
         pass
